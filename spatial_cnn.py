a17fbdb8d3d01a52688ee2db7d73532b2e630bdc
BenjaminLiu <imbenliu@gmail.com>
2019-09-10 16:01:10 -0700

    debug strach

--- spatial_cnn.py
+++ spatial_cnn.py
@@ -114,7 +114,7 @@ class Spatial_CNN():
             # save model
             if is_best:
                 self.best_prec1 = prec1
-                with open('record/spatial/spatial_video_preds_0.pickle','wb') as f:
+                with open('record/spatial/spatial_video_preds_x.pickle','wb') as f:
                     pickle.dump(self.dic_video_level_preds,f)
                 f.close()
 
@@ -123,7 +123,7 @@ class Spatial_CNN():
                 'state_dict': self.model.state_dict(),
                 'best_prec1': self.best_prec1,
                 'optimizer' : self.optimizer.state_dict()
-            },is_best,'record/spatial/checkpoint_0.pth.tar','record/spatial/model_best_0.pth.tar')
+            },is_best,'record/spatial/checkpoint_x.pth.tar','record/spatial/model_best_x.pth.tar')
 
     def train_1epoch(self):
         print('==> Epoch:[{0}/{1}][training stage]'.format(self.epoch, self.nb_epochs))
@@ -179,7 +179,7 @@ class Spatial_CNN():
                 'Prec@5':[round(top5.avg,4)],
                 'lr': self.optimizer.param_groups[0]['lr']
                 }
-        record_info(info, 'record/spatial/rgb_train_0.csv','train')
+        record_info(info, 'record/spatial/rgb_train_x.csv','train')
 
     def validate_1epoch(self):
         print('==> Epoch:[{0}/{1}][validation stage]'.format(self.epoch, self.nb_epochs))
@@ -221,7 +221,7 @@ class Spatial_CNN():
                 'Loss':[round(video_loss,5)],
                 'Prec@1':[round(video_top1,3)],
                 'Prec@5':[round(video_top5,3)]}
-        record_info(info, 'record/spatial/rgb_test.csv','test')
+        record_info(info, 'record/spatial/rgb_test_x.csv','test')
         return video_top1, video_loss
 
     def frame2_video_level_accuracy(self):
