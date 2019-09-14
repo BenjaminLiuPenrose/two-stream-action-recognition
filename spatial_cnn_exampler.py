d64a91393d5a4565829c7bf570f8196f8a1b7d4b
BenjaminLiu <imbenliu@gmail.com>
2019-09-14 10:47:04 -0700

    debug

--- spatial_cnn_exampler.py
+++ spatial_cnn_exampler.py
@@ -192,7 +192,7 @@ class Spatial_CNN():
             # loss = self.criterion(feature, target_var)
 
             # measure accuracy and record loss
-            prec1, prec5 = accuracy(output.data, label, lemniscate = self.lemniscate, trainloader = self.train_loader, sigma = self.arg.nce_t, topk=(1, 5))
+            prec1, prec5 = accuracy(feature.data, label, lemniscate = self.lemniscate, trainloader = self.train_loader, sigma = self.arg.nce_t, topk=(1, 5))
             losses.update(loss.data[0], data.size(0))
             top1.update(prec1[0], data.size(0))
             top5.update(prec5[0], data.size(0))
