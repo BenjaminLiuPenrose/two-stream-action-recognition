996416073038860934bc500c9edae24dc914d419
BenjaminLiu <imbenliu@gmail.com>
2019-09-14 10:41:00 -0700

    debug

--- spatial_cnn_exampler.py
+++ spatial_cnn_exampler.py
@@ -87,7 +87,7 @@ class Spatial_CNN():
         self.ndata = self.train_loader.__len__() * self.batch_size
         print ('==> Build model and setup loss and optimizer')
         #build model
-        self.model = resnet18(pretrained= True, channel=3, nb_classes = self.arg.low_dim).cuda()
+        self.model = resnet18(pretrained= True, nb_classes = self.arg.low_dim, channel=3).cuda()
         #Loss function and optimizer
         self.criterion = nn.CrossEntropyLoss().cuda()
         self.lemniscate = LinearAverageWithWeights(
