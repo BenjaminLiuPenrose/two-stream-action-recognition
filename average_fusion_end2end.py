ca74bbe5b624c78f14bac72378270f3a56bde5a3
BenjaminLiu <imbenliu@gmail.com>
2019-09-26 02:23:59 -0700

    debug

--- average_fusion_end2end.py
+++ average_fusion_end2end.py
@@ -40,17 +40,8 @@ def main():
     arg = parser.parse_args()
     print arg
 
-    rgb_preds='record/spatial/spatial_video_preds.pickle'
-    opf_preds = 'record/motion/motion_video_preds.pickle'
-
-    data_loader_spacial = dataloader.spatial_dataloader(
-                        BATCH_SIZE=arg.batch_size,
-                        num_workers=8,
-                        path='./data/jpegs_256/',
-                        ucf_list ='./UCF_list/',
-                        ucf_split ='01',
-                        )
-    data_loader_motion = dataloader.Motion_DataLoader(
+    #Prepare DataLoader
+    data_loader = dataloader.Motion_DataLoader(
                         BATCH_SIZE=arg.batch_size,
                         num_workers=8,
                         path='./data/tvl1_flow/',
@@ -59,25 +50,12 @@ def main():
                         in_channel=10,
                         )
 
-    train_loader_spacial, test_loader_spacial, test_video_spacial = data_loader_spacial.run()
-    train_loader_motion, test_loader_motion, test_video_motion = data_loader_motion.run()
-
-    model_spacial = Spatial_CNN(
-                        nb_epochs=arg.epochs,
-                        lr=arg.lr,
-                        batch_size=arg.batch_size,
-                        resume=arg.resume,
-                        start_epoch=arg.start_epoch,
-                        evaluate=arg.evaluate,
-                        train_loader=train_loader_spacial,
-                        test_loader=test_loader_spacial,
-                        test_video=test_video_spacial,
-                        end2end = True,
-    )
-    model_motion = Motion_CNN(
+    train_loader,test_loader, test_video = data_loader.run()
+    #Model
+    model = Fusion_CNN(
                         # Data Loader
-                        train_loader=train_loader_motion,
-                        test_loader=test_loader_motion,
+                        train_loader=train_loader,
+                        test_loader=test_loader,
                         # Utility
                         start_epoch=arg.start_epoch,
                         resume=arg.resume,
@@ -87,118 +65,10 @@ def main():
                         lr=arg.lr,
                         batch_size=arg.batch_size,
                         channel = 10*2,
-                        test_video=test_video_motion,
-                        end2end = True,
+                        test_video=test_video
                         )
-
-    model_spacial.build_model()
-    model_spacial.resume_and_evaluate()
-    model_motion.build_model()
-    model_motion.resume_and_evaluate()
-
-    cudnn.benchmark = True
-    best_prec1_spacial = 0
-    best_prec1_motion = 0
-    batch_time = AverageMeter()
-    top1_acc = AverageMeter()
-    top5_acc = AverageMeter()
-    end = time.time()
-    for epoch in range(arg.start_epoch, arg.epochs):
-        model_spacial.epoch = epoch
-        model_motion.epoch = epoch
-
-        model_spacial.train_1epoch()
-        model_motion.train_1epoch()
-
-        prec1_spacial, val_loss_spacial = model_spacial.validate_1epoch()
-        prec1_motion, val_loss_motion = model_motion.validate_1epoch()
-
-        # verify it is best or not
-        is_best_spacial = prec1_spacial > best_prec1_spacial
-        is_best_motion = prec1_motion > best_prec1_motion
-
-        # step scheduler
-        model_spacial.scheduler.step(val_loss_spacial)
-        model_motion.scheduler.step(val_loss_motion)
-
-        # store if it is best
-        if is_best_spacial:
-            best_prec1_spacial = prec1_spacial
-            with open(rgb_preds, 'wb') as f:
-                pickle.dump(model_spacial.dic_video_level_preds, f)
-            f.close()
-
-        save_checkpoint(
-            {
-                'epoch': epoch,
-                'state_dict': model_spacial.model.state_dict(),
-                'best_prec1': best_prec1_spacial,
-                'optimizer': model_spacial.optimizer.state_dict(),
-            },
-            is_best_spacial,
-            'record/spatial/checkpoint.pth.tar',
-            'record/spatial/model_best.pth.tar'
-            )
-
-        if is_best_motion:
-            best_prec1_motion = prec1_motion
-            with open(opf_preds, 'wb') as f:
-                pickle.dump(model_motion.dic_video_level_preds, f)
-            f.close()
-
-        save_checkpoint(
-            {
-                'epoch': epoch,
-                'state_dict': model_motion.model.state_dict(),
-                'best_prec1': best_prec1_motion,
-                'optimizer' : model_motion.optimizer.state_dict()
-            },
-            is_best_motion,
-            'record/motion/checkpoint.pth.tar',
-            'record/motion/model_best.pth.tar'
-            )
-
-        rgb = model_spacial.dic_video_level_preds
-        opf = model_motion.dic_video_level_preds
-        video_level_preds = np.zeros((len(rgb.keys()), 101))
-        video_level_preds_o = np.zeros((len(rgb.keys()), 101))
-        video_level_labels = np.zeros(len(rgb.keys()))
-        correct = 0
-        ii = 0
-        for name in sorted(rgb.keys()):
-            r = rgb[name]
-            o = opf[name]
-
-
-            label = int(test_video_spacial[name])-1
-
-            video_level_preds[ii, :] = r
-            video_level_preds_o[ii, :] = o
-            # video_level_preds[ii,:] = (r+o)
-            # video_level_preds[ii,:] = np.concatenate([r, o], 0).reshape((2, 101)).T
-            video_level_labels[ii] = label
-            ii+=1
-            if np.argmax(r+o) == (label):
-                correct += 1
-
-        video_level_labels = torch.from_numpy(video_level_labels).long()
-        video_level_preds = torch.from_numpy(video_level_preds).float()
-        video_level_preds_o = torch.from_numpy(video_level_preds_o).float()
-
-        top1,top5 = accuracy_tmp(video_level_preds, video_level_preds_o, video_level_labels, topk=(1,5))
-        batch_time.update(time.time() - end)
-        end = time.time()
-        top1_acc.update(top1)
-        top5_acc.update(top5)
-        video_loss = 0
-
-
-        info = {'Epoch':[epoch],
-                'Batch Time':[round(batch_time.avg,3)],
-                'Loss':[round(video_loss,5)],
-                'Prec@1':[round(top1_acc.avg,3)],
-                'Prec@5':[round(top5_acc.avg,3)]}
-        record_info(info, 'record/fusion_test_x.csv','test')
+    #Training
+    model.run()
 
 
 
